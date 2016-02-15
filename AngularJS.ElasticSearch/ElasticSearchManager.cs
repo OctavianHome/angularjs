52cfd71db18fa98983c1f2fca311da5b97a107ce
octavian <moc_tavy@yahoo.com>
2016-02-15 16:27:19 +0200

    Add ElasticSearch project

---
+++ AngularJS.ElasticSearch/ElasticSearchManager.cs
@@ -0,0 +1,24 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularJS.ElasticSearch
+{
+    public class ElasticSearchManager
+    {
+        private Lazy<ElasticClient> _ourAwesomeObject = new Lazy<ElasticClient>(() =>
+        {
+            var setting = new ConnectionSettings(new Uri("http://localhost:9200"));
+            //setting.SetDefaultIndex("musicstore");
+            return new ElasticClient(setting);
+        });
+
+        public ElasticClient OurAwesomeObject
+        {
+            get { return _ourAwesomeObject.Value; }
+        }
+    }
+}
